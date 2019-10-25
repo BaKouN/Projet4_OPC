57b38fd4c91eae3de5bc1b5b087d779185607556
Haroun BAKHOUCHE <darkopsis@gmail.com>
2019-10-25 10:16:48 +0200

    change of enviroment

--- Model/Manager.php
+++ Model/Manager.php
@@ -8,7 +8,7 @@ class Manager
 	}
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=localhost;charset=utf8', 'root', '');
         return $db;
     }
 }
